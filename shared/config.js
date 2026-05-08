b30c9af7e710690caa9c76e46a6f63a852fd72c4
raon2026 <skekfhks125455@gmail.com>
2026-05-08 13:29:24 +0900

    Update config.js

--- shared/config.js
+++ shared/config.js
@@ -1,4 +1,4 @@
 // 1) 아래 두 값만 당신 Supabase 프로젝트 값으로 유지/수정하세요.
 // - Project Settings > API 에서 확인 가능
-window.RAON_SUPABASE_URL = 'https://ylgbjeybpcdhpywbujdr.supabase.co/rest/v1/';
+window.RAON_SUPABASE_URL = 'https://ylgbjeybpcdhpywbujdr.supabase.co';
 window.RAON_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsZ2JqZXlicGNkaHB5d2J1amRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMzQ2NDAsImV4cCI6MjA5MzcxMDY0MH0.atE3boGdWrf0MfhFpMRDOoM1N2EIPVGfI0imAe3k7xw';
